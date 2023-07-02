646aa42e48fb9feb1e25ce5bff5e8615d17c0a3b
Paige <paigehcarroll@gmail.com>
2023-07-02 17:15:52 -0500

    I do not know what I have done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 // Assignment Code
+/*This code selects the button in the document and sets it to a variable value*/
 var generateBtn = document.querySelector('#generate');
 
-upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-numeric: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-special: ['!', '#', '$', '%', '&', '*', '+', '-', '/', '?', '@', '^', '~', '='] 
 
-//Clicking the button initiates the first prompt asking the user their desired password length
-generateBtn.addEventListener('click', function() {
-	var lengthprompt = prompt('Please choose password length(Must be between 8 and 128 characters).');
-	var passwordlength = lengthprompt;	
-	
+function writePassword() {
+	var password = generatePassword() {
+		var passwordlength = prompt('Please choose password length(Must be between 8 and 128 characters).');
+	//This if else statement collects true and false for what kinds of characters the user wants in their password
 	if (passwordlength >= 8 && passwordlength <= 128) {
 		var uppercase = confirm('Do you want your password to contain uppercase characters?');
 		var lowercase = confirm('Do you want your password to contain lowercase characters?');
@@ -20,20 +16,17 @@ generateBtn.addEventListener('click', function() {
 		alert('Please select a password length between 8 and 128 characters.');
 		}	
 	}
-
-);
-
-
-//Next need to use variables of true/false values in above function to determine what will be used to generate password
-
-// Write password to the #password input
-function writePassword() {
-	var password = generatePassword();
-	
 	var passwordText = document.querySelector('#password');
 
 	passwordText.value = password;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword); 
+};
+
+/*This code adds an event listener to the button that will initiate password creation once clicked*/
+generateBtn.addEventListener('click', writePassword);
+/*
+{
+upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+numeric: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+special: ['!', '#', '$', '%', '&', '*', '+', '-', '/', '?', '@', '^', '~', '='] } */
