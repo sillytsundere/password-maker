4277346b9e8dec5a17ad56087dfbf4820886365a
Paige <paigehcarroll@gmail.com>
2023-07-02 14:53:24 -0500

    updating js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+numeric: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+special: ['!', '#', '$', '%', '&', '*', '+', '-', '/', '?', '@', '^', '~', '='] 
+
+//Clicking the button initiates the first prompt asking the user their desired password length
+generateBtn.addEventListener('click', function() {
+	var lengthprompt = prompt('Please choose password length(Must be between 8 and 128 characters).');
+	var passwordlength = lengthprompt;	
+	
+	if (passwordlength >= 8 && passwordlength <= 128) {
+		var uppercase = confirm('Do you want your password to contain uppercase characters?');
+		var lowercase = confirm('Do you want your password to contain lowercase characters?');
+		var numeric = confirm('Do you want your password to contain numeric characters?');
+		var special = confirm('Do you want your password to contain special characters?');
+	} else {
+		alert('Please select a password length between 8 and 128 characters.');
+		}	
+	}
+
+);
+
+
+//Next need to use variables of true/false values in above function to determine what will be used to generate password
+
 // Write password to the #password input
 function writePassword() {
 	var password = generatePassword();
@@ -11,4 +36,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', writePassword); 
